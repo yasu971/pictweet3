4e3a1941523abbc60c6a1ac5c6a2c22ec6a921e9
yasu971 <yasuishii1421@gmail.com>
2021-04-10 10:42:42 +0900

    Update tweets_controller.rb

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+  
 
   def index
     @tweets = Tweet.all
@@ -18,17 +20,23 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
     tweet = Tweet.find(params[:id])
     tweet.update(tweet_params)
   end
-  
+
+  def show
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
   end
 
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+  
 end
