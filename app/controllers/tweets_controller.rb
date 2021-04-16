c7dd0ddf386cef312d6712d9568960a5c94acd34
yasu971 <yasuishii1421@gmail.com>
2021-04-16 13:01:54 +0900

    Update tweets_controller.rb
    投稿時に「name」を入力する必要がなくなったので、それに合わせてtweetsコントローラーの処理も変更。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,6 @@ class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
   before_action :move_to_index, except: [:index, :show]
   
-
   def index
     @tweets = Tweet.includes(:user)
   end
@@ -33,7 +32,7 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
   end
 
   def set_tweet
