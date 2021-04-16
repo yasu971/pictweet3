3334cbe320803ff3a421f06dcaf6e1abb87f8132
yasu971 <yasuishii1421@gmail.com>
2021-04-16 13:03:47 +0900

    rails db:migrate
    タイトルコマンド実行。

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_023033) do
+ActiveRecord::Schema.define(version: 2021_04_16_040243) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", precision: 6, null: false
