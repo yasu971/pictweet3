14b05d1f62f135b1610b819941c7289bd2b4509a
yasu971 <yasuishii1421@gmail.com>
2021-04-25 10:28:11 +0900

    Update user_spec.rb
    検証のためのインスタンスを作成。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
-      # nicknameが空では登録できないテストコードを記述します
+      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
     end
     it 'emailが空では登録できない' do
       # emailが空では登録できないテストコードを記述します
