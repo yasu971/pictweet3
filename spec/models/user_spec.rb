77c17e25ff1ee8dec60878f06bdf581f6b5eb474
yasu971 <yasuishii1421@gmail.com>
2021-04-26 10:47:09 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end  
+
   describe 'ユーザー新規登録' do
     it 'nicknameが空では登録できない' do
-      user = User.new(nickname: '', email: 'test@example', password: '000000', password_confirmation: '000000')
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.nickname = ''  # nicknameの値を空にする
+      user.nickname = ''
       user.valid?
       expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
     it 'emailが空では登録できない' do
-      user = User.new(nickname: 'test', email: '', password: '000000', password_confirmation: '000000')
-      user = FactoryBot.build(:user)  # Userのインスタンス生成
-      user.email = ''  # emailの値を空にする
+      user.email = ''
       user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
     end    
