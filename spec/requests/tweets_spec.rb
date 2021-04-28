de6d1cab4e5cab2752a6b308726e7d7d265d77d8
yasu971 <yasuishii1421@gmail.com>
2021-04-28 15:43:56 +0900

    Update tweets_spec.rb

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -22,6 +22,16 @@ describe TweetsController, type: :request do
       get root_path
       expect(response.body).to include('投稿を検索する')      
     end
-    
   end
+
+  describe 'GET #show' do
+    it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do 
+    end
+    it 'showアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do 
+    end
+    it 'showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する' do 
+    end
+  end   
 end
