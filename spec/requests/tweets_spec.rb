6283b889546de4035760300518fa08148d73cc53
yasu971 <yasuishii1421@gmail.com>
2021-04-28 15:45:23 +0900

    Update tweets_spec.rb

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -26,10 +26,16 @@ describe TweetsController, type: :request do
 
   describe 'GET #show' do
     it 'showアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      get tweet_path(@tweet)
+      expect(response.status).to eq 200      
     end
     it 'showアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do 
+      get tweet_path(@tweet)
+      expect(response.body).to include(@tweet.text)
     end
     it 'showアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do 
+      get tweet_path(@tweet)
+      expect(response.body).to include(@tweet.image)
     end
     it 'showアクションにリクエストするとレスポンスにコメント一覧表示部分が存在する' do 
     end
