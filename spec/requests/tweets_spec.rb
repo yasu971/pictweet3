716036c38734ed43f6037f40254fc9fe48156c85
yasu971 <yasuishii1421@gmail.com>
2021-04-28 15:43:29 +0900

    Update tweets_spec.rb

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -7,12 +7,21 @@ describe TweetsController, type: :request do
 
   describe 'GET #index' do
     it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do 
+      get root_path
+      expect(response.status).to eq 200
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do 
+      get root_path
+      expect(response.body).to include(@tweet.text)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do 
+      get root_path
+      expect(response.body).to include(@tweet.image)      
     end
     it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do 
+      get root_path
+      expect(response.body).to include('投稿を検索する')      
     end
+    
   end
 end
