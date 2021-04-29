5b572ddc2e839fbb16bf4049e704ea8bb0a4a897
yasu971 <yasuishii1421@gmail.com>
2021-04-29 10:02:10 +0900

    Update users_spec.rb

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -35,11 +35,22 @@ RSpec.describe 'ユーザー新規登録', type: :system do
   context 'ユーザー新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
       # トップページに移動する
+      visit root_path
       # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
       # 新規登録ページへ移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'Nickname', with: ''
+      fill_in 'Email', with: ''
+      fill_in 'Password', with: ''
+      fill_in 'Password confirmation', with: ''
       # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
       # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq user_registration_path
     end
   end
 end
